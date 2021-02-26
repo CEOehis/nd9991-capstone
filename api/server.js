cc082be3f4b18f486e17b9f6f6a447520f0f915e
Celestine Ekoh-Ordan <25967737+CEOehis@users.noreply.github.com>
2021-02-26 22:51:54 +0100

    Setup ESLint

--- api/server.js
+++ api/server.js
@@ -6,16 +6,17 @@ const PORT = 8080;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to the Header Parser Microservice'
+    message: 'Welcome to the Header Parser Microservice',
   });
 });
 
-app.get('/whoami', function (req, res) {
+app.get('/whoami', (req, res) => {
   const userDetails = {
-    ipaddress: req.header('x-forwarded-for') || req.connection.remoteAddress || req.socket.remoteAddress,
+    ipaddress:
+      req.header('x-forwarded-for') || req.connection.remoteAddress || req.socket.remoteAddress,
     language: req.headers['accept-language'].slice(0, req.headers['accept-language'].indexOf(',')),
     software: req.headers['user-agent'],
-  }
+  };
   res.json(userDetails);
 });
 
